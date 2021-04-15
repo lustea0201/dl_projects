0bc9c0fa2dd0b79895ed803d8e41698368fe32e9
reljadev <milan54321@hotmail.com>
2021-04-15 16:15:41 +0200

    Restructured model architecture

---
+++ project1/models/digit_classifier.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DigitClassifier(nn.Module):
+    def __init__(self, out_h, c1 = 32, c2 = 32, c3 = 64, h = 100, p = 0.3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, c1, kernel_size=3)
+        self.conv2 = nn.Conv2d(c1, c2, kernel_size=3)
+        self.conv3 = nn.Conv2d(c2, c3, kernel_size=2)
+        self.c3 = c3
+        self.fc1 = nn.Linear(c3, h)
+        self.drop = nn.Dropout(p)
+        self.fc2 = nn.Linear(h, out_h)
+        
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc1(x.view(-1, self.c3)))
+        x = self.drop(x)
+        x = self.fc2(x)
+        
+        return x 
--- project1/models/net2.py
+++ project1/models/net2.py
@@ -1,35 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from models.digit_classifier import DigitClassifier
 
 class Net2(nn.Module):
-    def __init__(self, h):
+    def __init__(self, c1 = 32, c2 = 32, c3 = 64, h = 100, p = 0.3):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=2)
-        self.fc1 = nn.Linear(64, h)
-        self.fc2 = nn.Linear(h, 10)
+        self.digit_classifier = DigitClassifier(10, c1, c2, c3, h, p)
         self.fc3 = nn.Linear(10, 1)
-        self.drop = nn.Dropout(p=0.3)
-        
-    def to_class_scores(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2))
-        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.fc1(x.view(-1, 64)))
-        x = self.drop(x)
-        x = self.fc2(x)
-        
-        return x 
 
     def forward(self, x):
         x1 = x[:, 0, :, :].unsqueeze(axis = 1)
         x2 = x[:, 1, :, :].unsqueeze(axis = 1)
         
-        
-        x1 = self.to_class_scores(x1)
-        x2 = self.to_class_scores(x2)
+        x1 = self.digit_classifier(x1)
+        x2 = self.digit_classifier(x2)
         
         xx2 = self.fc3(x2)
         xx1 = self.fc3(x1)
@@ -37,3 +22,5 @@ class Net2(nn.Module):
         x = (xx2-xx1).squeeze()
         
         return x, x1, x2
+    
+
