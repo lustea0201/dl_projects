9e5386219167761d7ecbb30943e6f51ea043ebfe
lustea0201 <alexandre.luster@epfl.ch>
2021-05-06 16:18:37 +0200

    Remove torch imports (except linspace...)

--- project2/utils.py
+++ project2/utils.py
@@ -1,19 +1,18 @@
-from torch import tensor, rand, sqrt, linspace, stack
+from torch import linspace, empty
 from matplotlib.pyplot import figure, scatter, legend, show
 from math import pi
 
 D = 2
-R_2 = tensor(1/(2*pi))
-CENTER = tensor([0.5, 0.5])
+R_2 = empty(1).fill_(1/(2*pi)).squeeze()
+CENTER = empty(2).fill_(0.5)
 
 def generate_dataset(nSamples = 1000,):
-    train_input = rand(nSamples*2, D)
+    train_input = empty((nSamples*2, D)).uniform_(0, 1)
     radii_2 = ((train_input-CENTER)**2).sum(axis = 1)
     train_target = ((R_2-radii_2).sign().long()+1)/2
 
 
-    train_target = stack([train_target, train_target], axis = 1)
-    train_target[:, 1] = 1 - train_target[:, 0]
+    train_target = train_target.unsqueeze(1)
 
     return train_input[:nSamples], train_target[:nSamples], train_input[nSamples:], train_target[nSamples:]
 
@@ -22,6 +21,6 @@ def show_dataset(X, y):
     pos = (y > 0)
     scatter(X[pos,0], X[pos,1], c = 'green')
     scatter(X[~pos,0], X[~pos,1], c = 'red')
-    scatter(CENTER[0]+linspace(0, sqrt(R_2), 50), [CENTER[1]]*50, c = 'blue')
+    scatter(CENTER[0]+linspace(0, R_2.sqrt(), 50), [CENTER[1]]*50, c = 'blue')
     legend(['1', '0', 'radius check'])
     show()
