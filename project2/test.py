b6e6a82f6d2fef294f4153c943ae6cac09b6a16d
lustea0201 <alexandre.luster@epfl.ch>
2021-05-06 16:18:56 +0200

    Transform 2D target to 1D

--- project2/test.py
+++ project2/test.py
@@ -2,12 +2,12 @@ from utils import generate_dataset
 from module import Linear, Sequential, ReLU, Tanh, LossMSE, LossNLL
 from torch import set_grad_enabled, manual_seed
 set_grad_enabled(False) # REQUIRED
-manual_seed(0)
+manual_seed(3)
 
 n_samples = 1000
 X_train, y_train, X_test, y_test = generate_dataset(n_samples)
 
-input_dim, h1, h2, h3, output_dim = 2, 25, 25, 25, 2
+input_dim, h1, h2, h3, output_dim = 2, 25, 25, 25, 1
 
 model = Sequential(Linear(input_dim, h1),
                    Tanh(),
@@ -16,10 +16,10 @@ model = Sequential(Linear(input_dim, h1),
                    Linear(h2, h3),
                    Tanh(),
                    Linear(h3, output_dim))
-LS = LossNLL()
+LS = LossMSE()
 nb_epochs = 100
 mini_batch_size = 50
-lr = 1e-5/n_samples*mini_batch_size # 1e-2 for MSE
+lr = 1e-2/n_samples*mini_batch_size # 1e-2 for MSE
 
 def train(nb_epochs, LS, lr):
     for epoch in range(nb_epochs):
@@ -41,12 +41,12 @@ def train(nb_epochs, LS, lr):
                     layer.W -= lr*layer.grad_W
                     layer.b -= lr*layer.grad_b
 
-        pred_train = model.forward(X_train).argmax(axis = 1)
+        pred_train = model.forward(X_train) > 0.5
 
-        target_train = y_train.argmax(axis = 1)
+        target_train = y_train
         accuracy_train = (pred_train == target_train).sum()/len(target_train)
-        pred_test = model.forward(X_test).argmax(axis = 1)
-        target_test = y_test.argmax(axis = 1)
+        pred_test = model.forward(X_test) > 0.5
+        target_test = y_test
         accuracy_test = (pred_test == target_test).sum()/len(target_test)
         print('Epoch {:d}: loss = {:.3f}, accuracy = {:.1f}% (train)/{:.1f}% (test))'.format(epoch, loss.item(),
                                                                                              accuracy_train*100,
