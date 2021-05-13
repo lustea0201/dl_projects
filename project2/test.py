f08537c530c1258fc3ee2739dc029b5a5d123e46
lustea0201 <alexandre.luster@epfl.ch>
2021-05-13 22:57:30 +0200

    Better results

--- project2/test.py
+++ project2/test.py
@@ -6,6 +6,10 @@ manual_seed(3)
 
 n_samples = 1000
 X_train, y_train, X_test, y_test = generate_dataset(n_samples)
+mean, std = X_train.mean(), X_train.std()
+
+X_train.sub_(mean).div_(std)
+X_test.sub_(mean).div_(std)
 
 input_dim, h1, h2, h3, output_dim = 2, 25, 25, 25, 1
 
@@ -39,9 +43,13 @@ def train(model, nb_epochs, LS, lr, mini_batch_size):
         target_test = y_test
         accuracy_test = (pred_test == target_test).sum()/len(target_test)
         print('Epoch {:d}: loss = {:.3f}, accuracy = {:.1f}% (train)/{:.1f}% (test))'.format(epoch, loss.item(),
-                                                                                             accuracy_train*100,
-                                                                                             accuracy_test*100))
+                                                                                         accuracy_train*100,
+                                                                                         accuracy_test*100))
+
+
 activation = Tanh
+
+
 model = Sequential(Linear(input_dim, h1),
                    activation(),
                    Linear(h1, h2),
@@ -52,7 +60,7 @@ model = Sequential(Linear(input_dim, h1),
 LS = LossMSE()
 nb_epochs = 100
 mini_batch_size = 50
-lr = 1e-3/n_samples*mini_batch_size # 1e-2 for MSE
+lr = 1e-3/n_samples*mini_batch_size
 
 train(model, nb_epochs, LS, lr, mini_batch_size)
 
@@ -69,6 +77,6 @@ model = Sequential(Linear(input_dim, h1),
 LS = LossBCE()
 nb_epochs = 100
 mini_batch_size = 50
-lr = 1e-3/n_samples*mini_batch_size # 1e-2 for MSE
+lr = 1e-3/n_samples*mini_batch_size
 
 train(model, nb_epochs, LS, lr, mini_batch_size)
