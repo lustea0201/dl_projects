b80718e30919c102e3c3f9fab0d9c07be2ff45b0
lustea0201 <alexandre.luster@epfl.ch>
2021-05-27 12:12:13 +0200

    Final modifications

--- project2/module.py
+++ project2/module.py
@@ -15,54 +15,54 @@ class Module(object):
 
 
 class Activation(Module):
-    """ Generic class for an activation function. 
+    """ Generic class for an activation function.
     """
-    
+
     def forward(self, input_):
-        """ Performs the forward pass for a generic activation function. 
-        Stores the input_ for the backward pass and call the function itself. 
-        
+        """ Performs the forward pass for a generic activation function.
+        Stores the input_ for the backward pass and call the function itself.
+
         Parameters
         ----------
         input_: torch.Tensor (any shape, typically (batch_size, dimension))
             The input to the activation function
-            
-            
+
+
         Returns
         -------
         output: torch.Tensor(same shape as input_)
             The output after passing through the activation
         """
-        
+
         output = self.f(input_)
         self.input = input_
-        
+
         return output
 
     def backward(self, grad_output):
-        """ Performs the backward pass for a generic activation function. 
-         
-        
+        """ Performs the backward pass for a generic activation function.
+
+
         Parameters
         ----------
         grad_output: torch.Tensor (same shape as self.input)
             The gradient at the output of the activation function
-            
-            
+
+
         Returns
         -------
         grad_input: torch.Tensor(same shape as self.input)
             The gradient at the input of the activation function
         """
-        
+
         grad_input = grad_output *  self.f_prime(self.input)
-        
+
         return grad_input
-    
+
     @staticmethod
     def f(self , *input_):
         raise NotImplementedError
-    
+
     @staticmethod
     def f_prime(self , *input_):
         raise NotImplementedError
@@ -73,125 +73,125 @@ class ReLU(Activation):
     """ Rectified Linear Unit class.
     Overrides f and f_prime.
     """
-    
+
     @staticmethod
     def f(x):
         """ Applies the ReLU function to x.
-         
-        
+
+
         Parameters
         ----------
         x: torch.Tensor (any shape, typically (batch_size, dimension))
             The input to the ReLU function
-            
-            
+
+
         Returns
         -------
         y: torch.Tensor(same shape as x)
             The result of applying ReLU to x
         """
-        
+
         y = x.clamp(min = 0)
-        
+
         return y
 
     @staticmethod
     def f_prime(x):
         """ Applies the derivative of ReLU to x.
-         
-        
+
+
         Parameters
         ----------
         x: torch.Tensor (any shape, typically (batch_size, dimension))
             The input to the ReLU derivative
-            
-            
+
+
         Returns
         -------
         y: torch.Tensor(same shape as x)
             The result of applying the derivative of ReLU to x
         """
-        
+
         y = (x.sign()+1)/2
-        
+
         return y
 
 
 class Tanh(Activation):
     """ Hyperbolic tangent class.
     Overrides f and f_prime.
-    
+
     Methods
     -------
     f(x):
         Apply the Tanh function to x.
-        
+
     f_prime(x):
         Apply the derivative of Tanh to x.
     """
-    
+
     @staticmethod
     def f(x):
         """ Applies the Tanh function to x.
-         
-        
+
+
         Parameters
         ----------
         x: torch.Tensor (any shape, typically (batch_size, dimension))
             The input to the Tanh function
-            
-            
+
+
         Returns
         -------
         y: torch.Tensor(same shape as x)
             The result of applying Tanh to x
         """
         y = x.tanh()
-        
+
         return y
 
     @staticmethod
     def f_prime(x):
         """ Applies the derivative of Tanh to x.
-         
-        
+
+
         Parameters
         ----------
         x: torch.Tensor (any shape, typically (batch_size, dimension))
             The input to the Tanh derivative
-            
-            
+
+
         Returns
         -------
         y: torch.Tensor(same shape as x)
             The result of applying the derivative of Tanh to x
         """
-        
+
         y = 4 * (x.exp() + x.mul(-1).exp()).pow(-2)
-        
+
         return y
 
 class LossMSE(Module):
-    """ Mean Squared Error Loss. 
+    """ Mean Squared Error Loss.
     """
-    
+
     def forward(self , prediction, target):
         """ Computes the MSE of a prediction given a target.
-        Stores the error for the backward pass. 
-        
+        Stores the error for the backward pass.
+
         Parameters
         ----------
         prediction: torch.Tensor of shape (batch_size, dimension)
             The predicted vectors
         target: torch.Tensor, same shape as prediction
             The target vectors
-            
+
         Returns
         -------
         loss: torch.Tensor of shape []
             The sum of the MSE loss of each batch
         """
-        
+
         error = target - prediction
         self.error = error
         self.d = prediction.shape[1]
@@ -201,36 +201,36 @@ class LossMSE(Module):
 
     def backward(self):
         """ Computes the derivative of the MSE loss with respect to its input.
-         
+
         Returns
         -------
         grad_input: torch.Tensor(same shape as self.error)
             The gradient with respect to the input of the loss function
         """
-        
+
         return -2*self.error/self.d
 
 class LossBCE(Module):
-    """ Binary Cross Entropy Loss. 
+    """ Binary Cross Entropy Loss.
     """
-    
+
     def forward(self , prediction, target):
         """ Computes the BCE of a prediction given a target.
-        Stores the prediction and target for the backward pass. 
-        
+        Stores the prediction and target for the backward pass.
+
         Parameters
         ----------
         prediction: torch.Tensor of shape (batch_size, 1)
             The predicted 1D vectors
         target: torch.Tensor, same shape as prediction
             The target 1D vectors, expected to be 0 or 1
-            
+
         Returns
         -------
         loss: torch.Tensor of shape []
             The sum of the MSE loss of each batch
         """
-        
+
         self.p = prediction
         self.t = target
         loss = (prediction.logaddexp(empty(prediction.shape).zero_()) - target*prediction).sum()/target.shape[1]
@@ -239,13 +239,13 @@ class LossBCE(Module):
 
     def backward(self):
         """ Computes the derivative of the MSE loss with respect to its input.
-         
+
         Returns
         -------
         grad_input: torch.Tensor(same shape as self.error)
             The gradient with respect to the input of the loss function
         """
-        
+
         exp = self.p.exp()
         grad_input = (exp/(1+exp) - self.t)/self.t.shape[1]
 
@@ -253,29 +253,29 @@ class LossBCE(Module):
 
 
 class Linear(Module):
-    """ Fully Connected Layer. 
+    """ Fully Connected Layer.
     """
-    
+
     def __init__(self, input_dim, output_dim, initialization = 'xavier'):
         """ Constructor
         Intializes the weight matrix and bias vector, set their gradient to 0 and declares the layer as trainable.
-        
+
         Parameters
         ----------
         input_dim: int
             The dimension of the input vectors
         output_dim: int
             The dimension of the output vectors
-        initialization: string ('normal' or 'xavier'), optional
+        initialization: string ('normal', 'xavier' or 'he'), optional
             The standard deviation of the normal distribution to initialize weights and biases.
         """
-        
+
         if (initialization == 'normal'):
             self.W = empty((output_dim, input_dim)).normal_()
         elif (initialization == 'xavier'):
               a = sqrt(6./(output_dim + input_dim))
               self.W = empty((output_dim, input_dim)).uniform_(-a, a)
-        elif (initialization == 'kaiming'):
+        elif (initialization == 'he'):
               self.W = empty((output_dim, input_dim)).normal_()*sqrt(2/input_dim)
         else:
             raise ValueError("Initialization strategy \"{:s}\" is not implemented".format(initialization))
@@ -291,39 +291,38 @@ class Linear(Module):
 
     def forward(self, input_):
         """ Computes the forward pass.
-        Stores the input_ for the backward pass. 
-        
+        Stores the input_ for the backward pass.
+
         Parameters
         ----------
         input_: torch.Tensor of shape (batch_size, input_dim)
             The input vectors
-            
+
         Returns
         -------
         output: torch.Tensor of shape (batch_size, output_dim)
             The output vectors
         """
-        
+
         self.input = input_
-        output = input_.mm(self.W.T) + self.b 
+        output = input_.mm(self.W.T) + self.b
 
         return output
 
 
     def backward(self, grad_output):
         """ Computes the backward pass.
-        
+
         Parameters
         ----------
         grad_output: torch.Tensor of shape (batch_size, output_dim)
             The gradient at the output of the fully connected layer
-            
+
         Returns
         -------
         grad_input: torch.Tensor of shape (batch_size, intput_dim)
             The gradient at the input of the fully connected layer
         """
-        
 
         grad_input = grad_output.mm(self.W)
         self.grad_W += grad_output.T.mm(self.input)
@@ -334,62 +333,62 @@ class Linear(Module):
 
     def param(self):
         """ Returns a list of 2 tuples: [(weights, weights gradient), (biases, biases gradient)]"""
-        
+
         return [(self.W, self.grad_W), (self.b, self.grad_b)]
 
 class Sequential(Module):
-    """ Sequential neural network 
+    """ Sequential neural network
     """
-    
+
     def __init__(self, *layers):
         """ Constructor.
-        
+
         Parameters
         ----------
         *layers: Modules
             All Modules in the network, given in sequential order
         """
-        
+
         self.layers = layers
 
     def forward(self , input_):
         """ Computes the forward pass through all layers.
-        
+
         Parameters
         ----------
         input_: torch.Tensor of shape (batch_size, input_dim)
             The input to the network
-            
+
         Returns
         -------
         output: torch.Tensor of shape (batch_size, output_dim)
             The output of the network
         """
-        
+
         output = input_
         for layer in self.layers:
             output = layer.forward(output)
-            
+
         return output
 
     def backward(self , grad_output):
         """ Computes the backward pass through all layers.
-        
+
         Parameters
         ----------
         grad_output: torch.Tensor of shape (batch_size, output_dim)
             The gradient at the output of the network, before the loss function
-            
+
         Returns
         -------
         grad_input: torch.Tensor of shape (batch_size, input_dim)
             The gradient at the input of the fully network
         """
-        
+
         grad_input = grad_output
         for layer in reversed(self.layers):
             grad_input = layer.backward(grad_input)
-            
+
         return grad_input
 
     def zero_grad(self):
@@ -398,5 +397,18 @@ class Sequential(Module):
             if layer.trainable:
                 layer.zero_grad()
 
+    def step(self, lr):
+        """ Perform one step of mini-batch SGD.
+
+		Parameters
+		----------
+		lr: float
+			Learning rate to tune the size of the step
+		"""
+        for layer in self.layers:
+                if layer.param(): # If non-empty parameter list
+                    layer.W -= lr*layer.grad_W
+                    layer.b -= lr*layer.grad_b
+
     def param(self):
-        return []
+        return [layer.param() for layer in self.layers]
--- project2/test.py
+++ project2/test.py
@@ -1,82 +1,34 @@
-from utils import generate_dataset
+from utils import generate_dataset, train
 from module import Linear, Sequential, ReLU, Tanh, LossBCE, LossMSE
 from torch import set_grad_enabled, manual_seed
 set_grad_enabled(False) # REQUIRED
-manual_seed(3)
+manual_seed(3) # For reproducibility
 
 n_samples = 1000
 X_train, y_train, X_test, y_test = generate_dataset(n_samples)
-mean, std = X_train.mean(), X_train.std()
 
+# Normalizing the data
+mean, std = X_train.mean(), X_train.std()
 X_train.sub_(mean).div_(std)
 X_test.sub_(mean).div_(std)
-
 input_dim, h1, h2, h3, output_dim = 2, 25, 25, 25, 1
 
 
 
-def train(model, nb_epochs, LS, lr, mini_batch_size):
-    for epoch in range(nb_epochs):
-        model.zero_grad()
-        for i in range(0, X_train.size(0), mini_batch_size):
-            input_ = X_train.narrow(0, i, mini_batch_size)
-            target = y_train.narrow(0, i, mini_batch_size)
-
-            # forward pass
-            out = model.forward(input_)
-            loss = LS.forward(out, target)
-            # backward pass
-            grad_output = LS.backward()
-
-            grad_input = model.backward(grad_output)
-
-            for layer in model.layers:
-                if layer.trainable:
-                    layer.W -= lr*layer.grad_W
-                    layer.b -= lr*layer.grad_b
-
-        pred_train = model.forward(X_train) > 0.5
-
-        target_train = y_train
-        accuracy_train = (pred_train == target_train).sum()/len(target_train)
-        pred_test = model.forward(X_test) > 0.5
-        target_test = y_test
-        accuracy_test = (pred_test == target_test).sum()/len(target_test)
-        print('Epoch {:d}: loss = {:.3f}, accuracy = {:.1f}% (train)/{:.1f}% (test))'.format(epoch, loss.item(),
-                                                                                         accuracy_train*100,
-                                                                                         accuracy_test*100))
 
 
 activation = Tanh
-
-
-model = Sequential(Linear(input_dim, h1),
+init = 'xavier'
+model = Sequential(Linear(input_dim, h1, init),
                    activation(),
-                   Linear(h1, h2),
+                   Linear(h1, h2, init),
                    activation(),
-                   Linear(h2, h3),
+                   Linear(h2, h3, init),
                    activation(),
-                   Linear(h3, output_dim))
+                   Linear(h3, output_dim, init))
 LS = LossMSE()
 nb_epochs = 100
 mini_batch_size = 50
 lr = 1e-3/n_samples*mini_batch_size
 
-train(model, nb_epochs, LS, lr, mini_batch_size)
-
-
-
-activation = ReLU
-model = Sequential(Linear(input_dim, h1),
-                   activation(),
-                   Linear(h1, h2),
-                   activation(),
-                   Linear(h2, h3),
-                   activation(),
-                   Linear(h3, output_dim))
-LS = LossBCE()
-nb_epochs = 100
-mini_batch_size = 50
-lr = 1e-3/n_samples*mini_batch_size
-
-train(model, nb_epochs, LS, lr, mini_batch_size)
+train(model, X_train, y_train, y_test, X_test, nb_epochs, LS, lr, mini_batch_size)
